e547f585403d7c8d1f9a27c0b68d48041c0f7211
Eduardo Rosas <lalorosas@outlook.com>
2019-12-09 20:56:46 -0600

    Solve bug 1

---
+++ WeatherApp/ViewModel/ValueConverters/BoolToRainConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.ViewModel.ValueConverters
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = (bool)value;
+
+            if (isRaining)
+                return "Currently raining"; 
+
+            return "Currently not raining";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string isRaining = (string)value;
+            if (isRaining != null)
+            {
+                if (isRaining == "Currently raining")
+                    return true;
+            }
+            return false;
+        }
+    }
+}
